3936b61d890a6637b050e04fefdcbb9833510e05
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-05-20 18:42:56 -0400

    Installed Nuget Package for entity framework and Created DB Context Class

---
+++ JWTAuthDotNet8/Data/UserDBContext.cs
@@ -0,0 +1,10 @@
+﻿using JWTAuthDotNet8.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuthDotNet8.Data
+{
+    public class UserDBContext(DbContextOptions<UserDBContext> options): DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
