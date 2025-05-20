9017d1615f0cf5d34da4c6b96db692800ea61215
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-05-19 23:06:53 -0400

    Add project files.

---
+++ JWTAuthDotNet8/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using JWTAuthDotNet8.Entities;
+using JWTAuthDotNet8.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthDotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user=new User();
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserModel userModel)
+        {
+            var hashPassword = new PasswordHasher<User>()
+                .HashPassword(user, userModel.Password);
+
+            user.Username=userModel.Username;
+            user.PasswordHash = hashPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserModel userModel)
+        {
+            if (user.Username != userModel.Username) 
+            {
+                return BadRequest("User Not Found");
+            }
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, userModel.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong Password");
+            }
+            string token = "success";
+            return Ok(token);
+        }
+    }
+}
