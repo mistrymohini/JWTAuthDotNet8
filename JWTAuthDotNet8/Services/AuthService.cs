bb710277c0084f42b66a8c2b429e714b16f95ad8
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-05-20 19:05:34 -0400

    Created AuthInterface and implemented it on class AuthService

---
+++ JWTAuthDotNet8/Services/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using JWTAuthDotNet8.Data;
+using JWTAuthDotNet8.Entities;
+using JWTAuthDotNet8.Models;
+
+namespace JWTAuthDotNet8.Services
+{
+    public class AuthService(UserDBContext context, IConfiguration configuration) : IAuthService
+    {
+        Task<string?> IAuthService.LoginAsync(UserModel request)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<User?> IAuthService.RegisterAsync(UserModel request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JWTAuthDotNet8/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using JWTAuthDotNet8.Entities;
+using JWTAuthDotNet8.Models;
+
+namespace JWTAuthDotNet8.Services
+{
+    public interface IAuthService
+    {
+        Task<User?> RegisterAsync(UserModel request);
+
+        Task<string?> LoginAsync(UserModel request);
+    }
+}
